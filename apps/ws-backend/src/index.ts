bd76836f1ad2c3e40b82d07fc47cf0d5932576e2
ahad <ahad@MacBookAir.lan>
2025-10-07 10:51:55 +0530

    gated the ws server with same jwt

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,23 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JsonWebTokenError, JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv'
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+  const url = request.url; // ws://localhost:8080?token=123123
+  if (!url) {
+    return
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  // ["ws:localhost:8080"] ["token=123123"]
+  const token = queryParams.get('token') || "";
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
 
   ws.on('message', function message(data) {
     ws.send('pong');
